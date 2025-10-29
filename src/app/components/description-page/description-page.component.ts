057c1e1e0aa08c2e0622afc0c2207ca907b767ec
Njabulo <sine22binda@gmail.com>
2025-10-29 13:38:56 +0200

    added a url for facebook and instergram icons on footer

--- src/app/components/description-page/description-page.component.ts
+++ src/app/components/description-page/description-page.component.ts
@@ -55,13 +55,12 @@ export class DescriptionPageComponent implements OnInit {
   }
 
   viewProduct(id: number) {
-    // Navigate to the product's description page
     this.router.navigate(['/products-page', id]);
   }
 
 //go to the product page
   goToProducts() {
-    const modalElement = document.getElementById('new');
+    const modalElement = document.getElementById("");
     if (modalElement) {
       const modal = bootstrap.Modal.getInstance(modalElement);
       modal?.hide();
